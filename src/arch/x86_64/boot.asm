5c4366390bfb2daf36c7baaabfb47a499a5df7ed
requinard <d.diks94@gmail.com>
2017-05-24 10:41:20 +0200

    feat(boot): init to true 64bit mode

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -1,4 +1,5 @@
 global start
+extern long_mode_start
 
 section .text
 bits 32
@@ -10,6 +11,11 @@ start:
 
   call set_up_page_tables
   call enable_paging
+
+  lgdt [gdt64.pointer]
+
+  jmp gdt64.code:long_mode_start
+
   mov dword [0xb8000], 0x2f4b2f4f
   hlt
 
@@ -131,6 +137,15 @@ enable_paging:
     mov cr0, eax
 
     ret
+
+section .rodata
+gdt64:
+  dq 0 ; zero entry
+.code equ $ - gdt64
+  dq (1<<43) | (1<<44) | (1<<47) | (1<<53) ; code segment
+.pointer:
+  dw $ - gdt64 - 1
+  dq gdt64
 
 section .bss
 align 4096
